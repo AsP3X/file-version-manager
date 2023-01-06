8f7bbb07c292100872d1182367c38702c17d9a6d
AsP3X <services@vorberg-webmail.de>
2023-01-06 22:26:53 +0100

    TASK: added installer

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# download the python script from the URL
+curl -o /usr/bin/fvm.py https://raw.githubusercontent.com/AsP3X/file-version-manager/master/fvm.py
+
+# make the script executable
+chmod +x /usr/bin/fvm.py
+
+# create a symlink to the script in /usr/bin
+ln -s /usr/bin/fvm.py /usr/bin/fvm
