86481ad8397fff310c439907b6f129ae2d4c612b
AsP3X <services@vorberg-webmail.de>
2023-01-06 23:13:05 +0100

    TASK: switch installer to use wget

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # download the python script from the URL
-curl -o /usr/bin/fvm.py https://raw.githubusercontent.com/AsP3X/file-version-manager/master/fvm.py?v=$(date +%s)
+wget --no-cache -O /usr/bin/fvm.py https://raw.githubusercontent.com/AsP3X/file-version-manager/master/fvm.py
 
 # make the script executable
 chmod +x /usr/bin/fvm.py
