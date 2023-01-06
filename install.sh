4f77688ce1905d50020cf77b0bf1f3ca277ae183
AsP3X <services@vorberg-webmail.de>
2023-01-06 23:40:20 +0100

    TASK: fixed installer script

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # download the python script from the URL
-wget --no-cache -O /usr/bin/fvm.py https://raw.githubusercontent.com/AsP3X/file-version-manager/master/fvm.py
+wget --no-cache -O /usr/bin/fvm.py https://raw.githubusercontent.com/AsP3X/file-version-manager/master/usr/bin/fvm
 
 # make the script executable
 chmod +x /usr/bin/fvm.py
